60f4cb2cbdece508197c95d4b8b07ce7f8cf495c
Chirag Jain <shreeparasnathexports@gmail.com>
2026-07-07 20:52:32 +0530

    modified family file in router folder

--- backend/api/app/routers/family.py
+++ backend/api/app/routers/family.py
@@ -16,3 +16,17 @@ def add_family(member: FamilyMember):
         "message": "Family member added successfully",
         "member": member
     }
+@router.delete("/{name}")
+def delete_family(name: str):
+    global family_db
+
+    for member in family_db:
+        if member.name == name:
+            family_db.remove(member)
+            return {
+                "message": "Family member deleted successfully"
+            }
+
+    return {
+        "message": "Member not found"
+    }
