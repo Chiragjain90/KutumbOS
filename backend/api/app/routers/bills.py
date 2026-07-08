543a989a30dfde8e2ca56aa8b91ccbe5008fb279
Chirag Jain <shreeparasnathexports@gmail.com>
2026-07-08 21:29:29 +0530

    modified bill file in router folder

--- backend/api/app/routers/bills.py
+++ backend/api/app/routers/bills.py
@@ -4,3 +4,31 @@ from schemas.bills import Bill
 router = APIRouter(prefix="/bills", tags=["Bills"])
 
 bills_db = []
+@router.get("/")
+def get_bills():
+    return bills_db
+@router.post("/")
+def add_bill(bill: Bill):
+    bills_db.append(bill)
+
+    return {
+        "message": "Bill added successfully",
+        "bill": bill
+    }
+@router.delete("/{id}")
+def delete_bill(id: int):
+
+    for bill in bills_db:
+        if bill.id == id:
+            bills_db.remove(bill)
+            return {"message": "Bill deleted"}
+
+    return {"message": "Bill not found"}
+@router.get("/total")
+def total_expense():
+
+    total = sum(bill.amount for bill in bills_db)
+
+    return {
+        "total_expense": total
+    }
