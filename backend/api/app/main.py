6b13328b8ee69250fc1217cd9af75f1b61f7edb1
Chirag Jain <shreeparasnathexports@gmail.com>
2026-07-08 21:16:25 +0530

    modified main file

--- backend/api/app/main.py
+++ backend/api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import health
 from routers import family
+from routers import shopping
 
 
 
@@ -9,6 +10,7 @@ app = FastAPI(title="KutumbOS API")
 app.include_router(health.router)
 
 app.include_router(family.router)
+app.include_router(shopping.router)
 
 @app.get("/")
 def root():
