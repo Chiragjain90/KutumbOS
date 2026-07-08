d7a7e65aef44bf23ef5f835f6c74e846ebe3aeda
Chirag Jain <shreeparasnathexports@gmail.com>
2026-07-08 21:24:52 +0530

    modified bill file in routers folder

--- backend/api/app/main.py
+++ backend/api/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers import health
 from routers import family
 from routers import shopping
+from routers import bills
 
 
 
@@ -11,6 +12,7 @@ app.include_router(health.router)
 
 app.include_router(family.router)
 app.include_router(shopping.router)
+app.include_router(bills.router)
 
 @app.get("/")
 def root():
